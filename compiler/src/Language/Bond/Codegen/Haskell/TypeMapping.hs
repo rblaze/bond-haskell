574c6be3e0b6c881b5fb855c8b1cdde675d97f4c
Andrey Sverdlichenko <blaze@ruddy.ru>
2016-08-24 21:54:15 +0000

    Add comment

--- compiler/src/Language/Bond/Codegen/Haskell/TypeMapping.hs
+++ compiler/src/Language/Bond/Codegen/Haskell/TypeMapping.hs
@@ -6,6 +6,8 @@ import Language.Bond.Syntax.Types
 import Language.Bond.Codegen.TypeMapping
 import Data.Text.Lazy.Builder
 
+-- Most of the fields are just use catchers.
+-- Can't use mapType for my own type mapping because it returns text, and haskell codegen needs AST type.
 haskellTypeMapping :: TypeMapping
 haskellTypeMapping = TypeMapping
     Nothing
