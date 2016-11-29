5dbc3d1792ea8e19298e8bf471f235ac38968488
Andrey Sverdlichenko <blaze@ruddy.ru>
2016-11-28 21:08:29 -0800

    Drop proxy records and generate functions for service methods

--- compiler/src/Language/Bond/Codegen/Haskell/ServiceDecl.hs
+++ compiler/src/Language/Bond/Codegen/Haskell/ServiceDecl.hs
@@ -13,65 +13,52 @@ serviceDecl :: CodegenOpts -> MappingContext -> ModuleName -> Declaration -> May
 serviceDecl opts ctx moduleName decl@Service{} = Just source
     where
     source = Module noLoc moduleName
-        [ LanguagePragma noLoc
-            [ Ident "Rank2Types" ]
-        ]
+        [ ]
         Nothing
-        Nothing -- XXX add exports?
+        Nothing
         imports
-        [proxyDecl, getFuncTypeSig, getFuncDecl]
+        (concatMap makeTypeSigAndFunc $ serviceMethods decl)
+
     monadName = Ident "m'"
     monadType = TyVar monadName
-    constrMonadIO = ClassA (implQual "MonadIO") [monadType]
-    serviceName = mkType $ makeDeclName decl
-    proxyName = let Ident baseName = serviceName in Ident (baseName ++ "Proxy")
-    typeParams = UnkindedVar monadName : map (\TypeParam{paramName} -> UnkindedVar $ mkVar paramName) (declParams decl)
-    imports = importInternalCommModule : map (\ m -> importTemplate{importModule = m}) fieldModules
-    proxyDecl = DataDecl noLoc DataType [] proxyName typeParams
-              [QualConDecl noLoc [] [] (RecDecl proxyName methodFields)]
-              []
-    fieldModules = unique $ filter (/= moduleName) $ filter (/= internalModuleAlias)
-                    $ concatMap (getTypeModules . snd) methodFields
-    methodFields = map mkMethod (serviceMethods decl)
-    mkMethod m@Function{} =
+    clientName = Ident "c'"
+    clientType = TyVar clientName
+    makeTypeSigAndFunc m = [makeTypeSig m, makeFunc m]
+
+    makeTypeSig m = TypeSig noLoc [mkVar $ makeMethodName m] $
+        TyForall Nothing
+            ( ClassA (implQual "CommClient") [monadType, clientType]
+            : map (typeParamConstraint $ implQual "BondStruct") (declParams decl)
+            )
+            (TyFun clientType $ methodType m)
+    eventResultType = TyApp monadType (TyTuple Boxed [])
+    methodType m@Event{} =
+        case methodInput m of
+            Nothing -> eventResultType
+            Just t -> TyFun (hsType (setType opts) ctx t) eventResultType
+    methodType m@Function{} =
         let resultType = case methodResult m of
                 Nothing -> TyCon $ implQual "Void"
                 Just t -> hsType (setType opts) ctx t
             funcResultType = TyApp monadType resultType
-            funcType = case methodInput m of
+         in case methodInput m of
                 Nothing -> funcResultType
                 Just t -> TyFun (hsType (setType opts) ctx t) funcResultType
-         in ([mkVar $ makeMethodName m], TyForall Nothing [constrMonadIO] funcType)
-    mkMethod m@Event{} =
-        let funcType = case methodInput m of
-                Nothing -> funcResultType
-                Just t -> TyFun (hsType (setType opts) ctx t) funcResultType
-            funcResultType = TyApp monadType (TyTuple Boxed [])
-         in ([mkVar $ makeMethodName m], TyForall Nothing [constrMonadIO] funcType)
-    getFuncName = Ident "get'proxy"
-    getFuncTypeSig = TypeSig noLoc [getFuncName] $
-        TyForall Nothing
-            ( ClassA (implQual "CommClient") [monadType, TyVar $ Ident "c'"]
-            : map (\TypeParam{paramName} -> ClassA (implQual "BondStruct") [TyVar $ mkVar paramName]) (declParams decl)
-            ) $
-            TyFun (TyVar $ Ident "c'") $
-                tyApp (TyCon $ UnQual proxyName) $
-                monadType : map (\TypeParam{paramName} -> TyVar $ mkVar paramName) (declParams decl)
+
     clientParam = Ident "client'"
-    getFuncDecl = simpleFun noLoc getFuncName clientParam $
-        RecConstr (UnQual proxyName) $ map makeClientCall (serviceMethods decl)
-    makeClientCall m@Function{methodInput = Nothing} = FieldUpdate (UnQual $ mkVar $ makeMethodName m) $
-        appFun (Var $ implQual "talk")
-            [ Var $ UnQual clientParam
-            , RecConstr (implQual "Void") []
-            ]
-    makeClientCall m@Function{} = FieldUpdate (UnQual $ mkVar $ makeMethodName m) $
+    makeFunc m@Function{methodInput = Nothing} =
+        simpleFun noLoc (mkVar $ makeMethodName m) clientParam $
+            appFun (Var $ implQual "talk")
+                [ Var $ UnQual clientParam
+                , RecConstr (implQual "Void") []
+                ]
+    makeFunc m@Function{} = simpleFun noLoc (mkVar $ makeMethodName m) clientParam $
         App (Var $ implQual "talk") (Var $ UnQual clientParam)
-    makeClientCall m@Event{} = FieldUpdate (UnQual $ mkVar $ makeMethodName m) $
+    makeFunc m@Event{} = simpleFun noLoc (mkVar $ makeMethodName m) clientParam $
         App (Var $ implQual "sendEvent") (Var $ UnQual clientParam)
 
-serviceDecl _ _ _ _ = error "serviceDecl called for invalid type"
+    imports = importInternalCommModule : map (\ m -> importTemplate{importModule = m}) methodModules
+    methodModules = unique $ filter (/= moduleName) $ filter (/= internalModuleAlias)
+                    $ concatMap (getTypeModules . methodType) (serviceMethods decl)
 
-tyApp :: L.Type -> [L.Type] -> L.Type
-tyApp con [] = con
-tyApp con (t:ts) = tyApp (TyApp con t) ts
+serviceDecl _ _ _ _ = error "serviceDecl called for invalid type"
